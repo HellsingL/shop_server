830064caf3862353decbfbc44ba1f3afdb223741
liangyuan <303832647@qq.com>
2018-08-14 09:23:40 +0800

    first commit

---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-container">
+    <!-- 登录框 -->
+    <div class="login-box">
+      <!-- 头部区域 -->
+      <div class="avatar-box">
+        <img src="../../assets/images/logo.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+</style>
